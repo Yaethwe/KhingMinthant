9ee47d29fe5a8e9fe6fc8f8101bbf2698e39e564
Yeaethwe <76844276+Yaethwe@users.noreply.github.com>
2022-11-30 21:02:22 +0630

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let leather = document.querySelector('#foot-mark')
+leather.style.opacity = 0.5
+leather.onmouseover=f=>{
+    leather.style.opacity = 1
+}
+
+leather.onmouseleave=f=>{
+    leather.style.opacity = 0.5
+}
+
